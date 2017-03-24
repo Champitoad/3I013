8d4f2a8d4e6a09fe94c346420167e2656b594334
Pablo Donato <pablo.donato@mailoo.org>
2017-03-24 13:06:04 +0100

    Update agent

--- interface.py
+++ interface.py
@@ -11,17 +11,15 @@ from autoencoder.predicter import Predicter
 yellow = '\033[93m'
 endc = '\033[0m'
 
-numgrid = NumGrid(size=(1,20), cursor_size=(10,10), digits={3})
-env = DirectionWrapper(numgrid)
-env.configure(num_steps=500, render_scale=1.5)
+numgrid = NumGrid(size=(10,10), cursor_size=(10,10), digits={1})
+env = DiscreteDirectionWrapper(numgrid)
+env.configure(num_steps=500, render_scale=2)
 
 agent = Predicter(learning_rate=0.001, nbp_input=np.prod(numgrid.cursor_size), time_training=1000)
 
 for i_episode in range(5):
     print("\n********* EPISODE", i_episode, "**********\n")
-    env.env.cursor_pos = np.array((10,0))
-    env.env.steps = 0
-    observation = None
+    observation = env.reset()
     reward = None
     done = False
     info = {"cursor": env.env.cursor}
