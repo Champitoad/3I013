b52fb45b825d368f8e4d7726481123dfdc31a042
Pablo Donato <pablo.donato@mailoo.org>
2017-04-10 19:15:59 +0200

    Clean training procedure

--- interface.py
+++ interface.py
@@ -8,11 +8,11 @@ from gym_numgrid.envs import NumGrid
 from gym_numgrid.wrappers import *
 from autoencoder.predicter import Predicter
 
-numgrid = NumGrid(size=(5,5), cursor_size=(8,8), digits={1}, num_steps=100)
+numgrid = NumGrid(size=(5,5), cursor_size=(12,12), digits={0}, num_steps=100)
 env = DiscreteDirectionWrapper(numgrid)
 
-agent = Predicter(learning_rate=0.001, nbp_input=np.prod(numgrid.cursor_size), move_distance=10)
-agent.load_model("models/predicter0.ckpt")
+pred = Predicter(numgrid.cursor_size)
+pred.load_model("models/predicter0.ckpt")
 
 mean_acc = 0
 total_mean_acc = 0
@@ -22,12 +22,11 @@ for i_episode in range(100):
     done = False
     info = {}
     image = observation.reshape(1,-1).astype(np.float32) / 255
-    action = agent.act(observation, reward, done, info)
     while not done:
-        observation, reward, done, info = env.step(action)
         action = (10, env.direction_space.sample())
+        observation, reward, done, info = env.step(action)
         next_image = observation.reshape(1,-1).astype(np.float32) / 255
-        mean_acc += agent.accuracy(image, action[1], next_image)
+        mean_acc += pred.accuracy(image, action[1], next_image)
         image = next_image
     mean_acc /= numgrid.num_steps
     total_mean_acc += mean_acc
--- train_subnet.py
+++ train_subnet.py
@@ -3,23 +3,25 @@ sys.path.append("gym-numgrid")
 sys.path.append("agent")
 
 import numpy as np
+from multiprocessing import Process
 from gym_numgrid.envs import NumGrid
 from gym_numgrid.wrappers import *
 from autoencoder.predicter import Predicter
 
-numgrid = NumGrid(size=(5,5), cursor_size=(8,8), digits={0}, num_steps=100)
-env = DiscreteDirectionWrapper(numgrid)
+grid_size = (10,10)
+cursor_size = (12,12)
+num_steps = 100
+num_episodes = 100
 
-agent = Predicter(learning_rate=0.001, nbp_input=np.prod(numgrid.cursor_size), move_distance=10)
+def train(digit):
+    numgrid = NumGrid(size=grid_size, cursor_size=cursor_size, digits={digit}, num_steps=num_steps)
+    numgrid = DiscreteDirectionWrapper(numgrid)
+    pred = Predicter(cursor_size)
+    pred.learn(numgrid, num_episodes)
+    pred.save_model("models/predicter{}.ckpt".format(digit))
 
-for i_episode in range(100):
-    observation = env.reset()
-    reward = None
-    done = False
-    info = {}
-    action = agent.act(observation, reward, done, info)
-    while not done:
-        observation, reward, done, info = env.step(action)
-        action = agent.act(observation, reward, done, info)
-
-agent.save_model("models/predicter0.ckpt")
+jobs = [Process(target=train, args=(digit,)) for digit in range(10)]
+for job in jobs:
+    job.start()
+for job in jobs:
+    job.join()
