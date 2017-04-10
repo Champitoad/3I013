04881f16e6741dc2819775b5b0e88a4aac3a16b6
Pablo Donato <pablo.donato@mailoo.org>
2017-04-10 16:53:50 +0200

    Cursor observation space changed to cursor view

--- interface.py
+++ interface.py
@@ -8,10 +8,7 @@ from gym_numgrid.envs import NumGrid
 from gym_numgrid.wrappers import *
 from autoencoder.predicter import Predicter
 
-yellow = '\033[93m'
-endc = '\033[0m'
-
-numgrid = NumGrid(size=(50,50), cursor_size=(8,8), digits={0}, num_steps=1000)
+numgrid = NumGrid(size=(5,5), cursor_size=(8,8), digits={1}, num_steps=100)
 env = DiscreteDirectionWrapper(numgrid)
 
 agent = Predicter(learning_rate=0.001, nbp_input=np.prod(numgrid.cursor_size), move_distance=10)
@@ -23,16 +20,16 @@ for i_episode in range(100):
     observation = env.reset()
     reward = None
     done = False
-    info = {"cursor": env.env.cursor}
-    image = info['cursor'].reshape(1,-1).astype(np.float32) / 255
+    info = {}
+    image = observation.reshape(1,-1).astype(np.float32) / 255
     action = agent.act(observation, reward, done, info)
     while not done:
         observation, reward, done, info = env.step(action)
-        action = (10,env.direction_space.sample())
-        next_image = info['cursor'].reshape(1,-1).astype(np.float32) / 255
+        action = (10, env.direction_space.sample())
+        next_image = observation.reshape(1,-1).astype(np.float32) / 255
         mean_acc += agent.accuracy(image, action[1], next_image)
         image = next_image
     mean_acc /= numgrid.num_steps
     total_mean_acc += mean_acc
 total_mean_acc /= 100
-print("Total mean accuracy: {}%".format(total_mean_acc))
+print("Total mean accuracy: {}".format(total_mean_acc))
--- train_subnet.py
+++ train_subnet.py
@@ -7,7 +7,7 @@ from gym_numgrid.envs import NumGrid
 from gym_numgrid.wrappers import *
 from autoencoder.predicter import Predicter
 
-numgrid = NumGrid(size=(50,50), cursor_size=(8,8), digits={0}, num_steps=1000)
+numgrid = NumGrid(size=(5,5), cursor_size=(8,8), digits={0}, num_steps=100)
 env = DiscreteDirectionWrapper(numgrid)
 
 agent = Predicter(learning_rate=0.001, nbp_input=np.prod(numgrid.cursor_size), move_distance=10)
@@ -16,7 +16,7 @@ for i_episode in range(100):
     observation = env.reset()
     reward = None
     done = False
-    info = {"cursor": env.env.cursor}
+    info = {}
     action = agent.act(observation, reward, done, info)
     while not done:
         observation, reward, done, info = env.step(action)
