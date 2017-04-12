0678cd1d4b1fe9ba060a514f40999cb4a399c75c
Pablo Donato <pablo.donato@mailoo.org>
2017-04-12 17:49:33 +0200

    Add evidence accumulation (encouraging results)

--- agent/random_predicter.py
+++ agent/random_predicter.py
@@ -14,10 +14,17 @@ class RandomPredicterAgent(Agent):
     """
     Agent for NumGrid moving in random directions, and labelling
     using 10 predicters (one for each digit).
+    
+    The agent cumulates evidence from the predicters before trying a guess of the digit.
     """
-    def __init__(self, numgrid):
+    def __init__(self, numgrid, score_thr):
+        """
+        numgrid -- NumGrid environment to evaluate
+        score_thr -- decision threshold for the predicters cumulated score
+        """
         super().__init__(numgrid, episode_max_length=numgrid.num_steps)
 
+        # Load predicter models from disk
         self.preds = []
         for digit in range(10):
             with tf.Graph().as_default():
@@ -26,6 +33,8 @@ class RandomPredicterAgent(Agent):
             self.preds.append(pred)
 
         self.image = None
+        self.score_thr = score_thr
+        self.score = np.zeros(10) # Cumulated score for each predicter
 
     def act(self, observation):
         direction = random.choice(tuple(directions))
@@ -39,10 +48,12 @@ class RandomPredicterAgent(Agent):
         accuracy = lambda pred: pred.accuracy(self.image, direction, next_image)
         with ThreadPool(10) as p:
             accs = p.map(accuracy, self.preds)
-        # print(accs)
-        accs = np.array(accs)
-        accs = accs[accs > 0.80]
-        if len(accs) > 0:
-            digit = np.argmax(accs)
+        pred_digit = np.argmax(accs)
+        self.score[pred_digit] += 1
+        for d in range(10):
+            if self.score[d] == self.score_thr:
+                digit = d
+                self.score = np.zeros(10)
 
+        self.image = next_image
         return (digit, direction)
--- interface.py
+++ interface.py
@@ -10,18 +10,28 @@ from random_predicter import RandomPredicterAgent
 from consts import *
 
 grid_size = (1,10)
-num_steps = 40
+num_steps = 20
 num_episodes = 10
 
 numgrid = NumGrid(size=grid_size, cursor_size=cursor_size, num_steps=num_steps)
 numgrid = DiscreteDirectionWrapper(numgrid)
 
-agent = RandomPredicterAgent(numgrid)
+agent = RandomPredicterAgent(numgrid, score_thr=5)
 
 score = 0
+num_preds = 0
+num_ok = 0
 for episode in range(num_episodes):
-    score += agent.get_trajectory(render=True)['reward'].sum()
+    traj = agent.get_trajectory(render=True)
+    for i in range(num_steps):
+        rew = traj['reward'][i]
+        if rew != 0:
+            score += rew
+            num_preds += 1
+            if rew > 0:
+                num_ok += 1
 max_score = 3 * num_steps * num_episodes
-accuracy = (score + max_score) / (2 * max_score)
-print("Score: {}".format(score))
-print("Accuracy: {}%".format(accuracy*100))
+accuracy = num_ok / num_preds * 100
+print("Score range: [{};{}]".format(-max_score, max_score))
+print("Final score: {}".format(score))
+print("Accuracy: {:.4g}%".format(accuracy))
